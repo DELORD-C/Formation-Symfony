15f7b7863b53de46877b49c543c469b003edc900
Clément <clement.delord@epitech.eu>
2022-10-06 14:22:24 +0200

    Sticky locale

--- src/Subscriber/LocaleSetterSubscriber.php
+++ src/Subscriber/LocaleSetterSubscriber.php
@@ -2,6 +2,7 @@
 
 namespace App\Subscriber;
 
+use JetBrains\PhpStorm\ArrayShape;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\KernelEvent;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
@@ -9,20 +10,33 @@ use Symfony\Component\HttpKernel\KernelEvents;
 
 class LocaleSetterSubscriber implements EventSubscriberInterface
 {
+    public array $allowedLocales;
 
-    public function onKernelRequest(RequestEvent $event) {
-
-        $request = $event->getRequest();
-
-        $allowedLocales = ['en', 'fr'];
-        $clientLanguage = substr($request->headers->get('Accept-Language'), 0, 2);
-
-        if ($clientLanguage != null && in_array($clientLanguage, $allowedLocales)) {
-            $request->setLocale($clientLanguage);
-        }
+    function __construct() {
+        //The first one is default
+        $this->allowedLocales = [
+            'en',
+            'fr'
+        ];
     }
 
-    public static function getSubscribedEvents()
+    public function onKernelRequest(RequestEvent $event)
+    {
+        $request = $event->getRequest();
+
+        if (!$locale = $request->getSession()->get('_locale')) {
+            $clientLanguage = substr($request->headers->get('Accept-Language'), 0, 2);
+            if ($clientLanguage != null && in_array($clientLanguage, $this->allowedLocales)) {
+                $locale = $clientLanguage;
+            } else {
+                $locale = $this->allowedLocales[0];
+            }
+        }
+        $request->setLocale($locale);
+    }
+
+    #[ArrayShape([KernelEvents::REQUEST => "array[]"])]
+    public static function getSubscribedEvents(): array
     {
         return [
             KernelEvents::REQUEST => [['onKernelRequest', 25]]
