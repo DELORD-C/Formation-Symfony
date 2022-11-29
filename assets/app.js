29abee34e0c18c6f7095d7c612f931e78743f20e
Clément DELORD <clement.delord@epitech.eu>
2022-11-29 15:14:03 +0100

    bootstrap style

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,7 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
-require('bootstrap');
+require('bootstrap/dist/js/bootstrap');
 
 // start the Stimulus application
 import './bootstrap';
