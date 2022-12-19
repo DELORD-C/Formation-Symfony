3615922081145b4c732b7ae89c3bd243de79f096
Clément DELORD <clement.delord@epitech.eu>
2022-12-19 14:46:40 +0100

    route param random

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -15,4 +15,12 @@ class DefaultController extends AbstractController
             'Hello World !'
         );
     }
+
+    #[Route('/random/{min}/{max}', requirements: ['min' => '\d+', 'max' => '\d+'])]
+    public function random(int $min, int $max): Response
+    {
+        return new Response(
+            random_int($min, $max)
+        );
+    }
 }
