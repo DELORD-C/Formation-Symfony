05448ce6c46427d742fa1456298826eb436573a1
Clément DELORD <clement.delord@epitech.eu>
2022-12-19 14:59:31 +0100

    route param prio

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -16,11 +16,28 @@ class DefaultController extends AbstractController
         );
     }
 
-    #[Route('/random/{min}/{max}', requirements: ['min' => '\d+', 'max' => '\d+'])]
-    public function random(int $min, int $max): Response
+    #[Route(
+        '/random/{min}/{max}',
+        requirements: ['min' => '\d+', 'max' => '\d+'],
+        methods: ['GET', 'HEAD']
+    )]
+    public function random(int $min = 0, int $max = 1000): Response
     {
         return new Response(
             random_int($min, $max)
+        );
+    }
+
+    #[Route(
+        '/random/999/{min}',
+        requirements: ['min' => '\d+'],
+        methods: ['GET', 'HEAD'],
+        priority: 1
+    )]
+    public function randomInf(int $min): Response
+    {
+        return new Response(
+            random_int($min, 9999999999999999999999999999999999999999999)
         );
     }
 }
