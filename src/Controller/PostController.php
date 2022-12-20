a06ba2fd656beb534b8af1ac7af35c2b0a95f7fe
Clément DELORD <clement.delord@epitech.eu>
2022-12-20 13:54:40 +0100

    facto

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -8,15 +8,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
-    #[Route('/post/create', methods: ['GET', 'HEAD'])]
-    public function create (): Response
+    #[Route('/post/create')]
+    public function create (Request $request, ManagerRegistry $doctrine): Response
     {
         $post = new Post();
         $form = $this->createFormBuilder($post)
@@ -24,21 +23,19 @@ class PostController extends AbstractController
             ->add('body', TextareaType::class)
             ->add('save', SubmitType::class)
             ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($post);
+            $entityManager->flush();
+            return $this->redirectToRoute("app_post_create");
+        }
+
         return $this->render('post/create.html.twig', [
             'form' => $form
         ]);
-    }
-
-    #[Route('/post/create', methods: ['POST'])]
-    public function store (Request $request, ManagerRegistry $doctrine): Response
-    {
-        $entityManager = $doctrine->getManager();
-        $post = new Post();
-        $post->setSubject($request->get('subject'));
-        $post->setBody($request->get('body'));
-        $entityManager->persist($post);
-        $entityManager->flush();
-
-        return new RedirectResponse('/post/create');
     }
 }
