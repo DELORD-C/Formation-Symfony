6e6606cbac259fc1bef84d8600a7cb2168ed4404
Clément DELORD <clement.delord@epitech.eu>
2022-12-20 12:36:13 +0100

    form create handle

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,15 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
-    #[Route('/post/create')]
+    #[Route('/post/create', methods: ['GET', 'HEAD'])]
     public function create (): Response
     {
         return $this->render('post/create.html.twig');
+    }
+
+    #[Route('/post/create', methods: ['POST'])]
+    public function store (Request $request, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $post = new Post();
+        $post->setSubject($request->get('subject'));
+        $post->setBody($request->get('body'));
+        $entityManager->persist($post);
+        $entityManager->flush();
+
+        return new RedirectResponse('/post/create');
     }
 }
