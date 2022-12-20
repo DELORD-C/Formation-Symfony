6f2ce587642f8a47f87d6865bfeaddf33083c2d6
Clément DELORD <clement.delord@epitech.eu>
2022-12-20 13:40:15 +0100

    formBuilder in controller

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use App\Entity\Post;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,7 +18,15 @@ class PostController extends AbstractController
     #[Route('/post/create', methods: ['GET', 'HEAD'])]
     public function create (): Response
     {
-        return $this->render('post/create.html.twig');
+        $post = new Post();
+        $form = $this->createFormBuilder($post)
+            ->add('subject', TextType::class)
+            ->add('body', TextareaType::class)
+            ->add('save', SubmitType::class)
+            ->getForm();
+        return $this->render('post/create.html.twig', [
+            'form' => $form
+        ]);
     }
 
     #[Route('/post/create', methods: ['POST'])]
