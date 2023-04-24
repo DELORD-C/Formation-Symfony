0dbca23ff0476b0d9c49de411bb1968e4c8d08a4
Clément DELORD <clement.delord@epitech.eu>
2023-04-24 12:14:39 +0200

    Validation de parametre & nom de route

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,14 +8,20 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/number/{min}/{max}')]
+    #[Route(
+        '/number/{min}/{max}',
+        requirements: [
+            'min' => '\d+', 'max' => '\d+'
+        ],
+        name: "number"
+    )]
     function number (int $min = 0, int $max = 100): Response
     {
         $number = random_int($min, $max);
         return new Response($number);
     }
 
-    #[Route('/number/fake', priority: 1)]
+    #[Route('/number/fake')]
     function numberFake (): Response
     {
         return new Response(10000);
