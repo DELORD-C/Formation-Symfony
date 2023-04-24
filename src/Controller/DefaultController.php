b9321bae8862cc1e1c988626dfe9e3a6f6d0a0f1
Clément DELORD <clement.delord@epitech.eu>
2023-04-24 12:05:54 +0200

    annotations + priority

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,12 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    function number (): Response
+    #[Route('/number/{min}/{max}')]
+    function number (int $min = 0, int $max = 100): Response
     {
-        $number = random_int(0, 100);
+        $number = random_int($min, $max);
         return new Response($number);
+    }
+
+    #[Route('/number/fake', priority: 1)]
+    function numberFake (): Response
+    {
+        return new Response(10000);
     }
 }
