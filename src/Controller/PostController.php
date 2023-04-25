e1613a070ea03d08ad33633d138863ff1ddcb1e2
Clément DELORD <clement.delord@epitech.eu>
2023-04-25 10:36:37 +0200

    Post Edit & List

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Post;
+use App\Repository\PostRepository;
 use App\Form\PostType;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -27,6 +28,35 @@ class PostController extends AbstractController {
             $em->flush();
             $this->addFlash('notice', 'Post successfully created!');
             return $this->redirect('/post/create');
+        }
+
+        return $this->render('post/create.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/list')]
+    function list (PostRepository $postRepository)
+    {
+        $posts = $postRepository->findAll();
+        return $this->render('post/list.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+
+    #[Route('/edit/{post}')]
+    function edit (Post $post, Request $request, EntityManagerInterface $em)
+    {
+        $form = $this->createForm(PostType::class, $post);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $em->persist($post);
+            $em->flush();
+            $this->addFlash('notice', 'Post successfully edited!');
+            return $this->redirect($request->getUri());
         }
 
         return $this->render('post/create.html.twig', [
