630eaeeea2c6bdd4aae332104a12bfea7daa1702
Clément DELORD <cdelord@dawan.fr>
2023-10-02 14:16:28 +0200

    CRUD Post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,11 +14,55 @@ use Symfony\Component\Routing\Annotation\Route;
 class PostController extends AbstractController
 {
     #[Route('/post/create')]
-    public function create(Request $request): Response
+    public function create(Request $request, EntityManagerInterface $em): Response
     {
 
+        $form = $this->createForm(PostType::class);
 
+        $form->handleRequest($request);
 
-        return $this->render('Post/create.html.twig');
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $em->persist($post);
+            $em->flush();
+        }
+
+        return $this->render('Post/create.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/post/list')]
+    public function list(PostRepository $rep): Response
+    {
+        $posts = $rep->findAll();
+
+        return $this->render('Post/list.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+
+    #[Route('/post/update/{post}')]
+    public function update(Post $post, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(PostType::class, $post);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+        }
+
+        return $this->render('Post/create.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/post/delete/{post}')]
+    public function delete(Post $post, EntityManagerInterface $em): Response
+    {
+        $em->remove($post);
+        $em->flush();
+        return $this->redirectToRoute("app_post_list");
     }
 }
--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -2,11 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PostType extends AbstractType
 {
@@ -17,5 +19,12 @@ class PostType extends AbstractType
             ->add('body', TextareaType::class)
             ->add('submit', SubmitType::class)
         ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class
+        ]);
     }
 }
