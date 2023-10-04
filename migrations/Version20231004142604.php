c46881a69fecb1e031dead5efa4283e0b96f0817
Clément DELORD <cdelord@dawan.fr>
2023-10-04 16:33:10 +0200

    api

---
+++ migrations/Version20231004142604.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231004142604 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TEMPORARY TABLE __temp__post AS SELECT id, user_id, subject, body, created_at FROM post');
+        $this->addSql('DROP TABLE post');
+        $this->addSql('CREATE TABLE post (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, user_id INTEGER NOT NULL, subject VARCHAR(255) NOT NULL, body VARCHAR(1000) NOT NULL, created_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , CONSTRAINT FK_5A8A6C8DA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO post (id, user_id, subject, body, created_at) SELECT id, user_id, subject, body, created_at FROM __temp__post');
+        $this->addSql('DROP TABLE __temp__post');
+        $this->addSql('CREATE INDEX IDX_5A8A6C8DA76ED395 ON post (user_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TEMPORARY TABLE __temp__post AS SELECT id, user_id, subject, body, created_at FROM post');
+        $this->addSql('DROP TABLE post');
+        $this->addSql('CREATE TABLE post (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, user_id INTEGER DEFAULT NULL, subject VARCHAR(255) NOT NULL, body VARCHAR(1000) NOT NULL, created_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , CONSTRAINT FK_5A8A6C8DA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO post (id, user_id, subject, body, created_at) SELECT id, user_id, subject, body, created_at FROM __temp__post');
+        $this->addSql('DROP TABLE __temp__post');
+        $this->addSql('CREATE INDEX IDX_5A8A6C8DA76ED395 ON post (user_id)');
+    }
+}
