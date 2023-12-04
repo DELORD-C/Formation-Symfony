2514c82cf7501d6702ae5cd5ad9ad001e93dd616
DELORD Clément <cdelord@dawan.fr>
2023-12-04 17:08:42 +0100

    CRUD

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 use App\Entity\Post;
 use App\Form\PostType;
+use App\Repository\PostRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,6 +17,46 @@ class PostController extends AbstractController {
     {
         $post = new Post();
 
+        $form = $this->createForm(PostType::class, $post);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $em->persist($post);
+            $em->flush();
+            return $this->redirectToRoute('app_post_list');
+        }
+
+        return $this->render('Post/create.html.twig', [
+            'postForm' => $form
+        ]);
+    }
+
+    #[Route('/list')]
+    public function list (PostRepository $rep): Response
+    {
+        $posts = $rep->findAll();
+        return $this->render('Post/list.html.twig', ['posts' => $posts]);
+    }
+
+    #[Route('/read/{post}')]
+    public function read (Post $post): Response
+    {
+        return $this->render('Post/read.html.twig', ['post' => $post]);
+    }
+
+    #[Route('/delete/{post}')]
+    public function delete (Post $post, EntityManagerInterface $em): Response
+    {
+        $em->remove($post);
+        $em->flush();
+        return $this->redirectToRoute('app_post_list');
+    }
+
+    #[Route('/update/{post}')]
+    public function update (Post $post, Request $request, EntityManagerInterface $em): Response
+    {
         $form = $this->createForm(PostType::class, $post);
 
         $form->handleRequest($request);
