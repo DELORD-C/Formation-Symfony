e98b95ddd6927365117e8542f87f7b3aa0e6d214
DELORD Clément <cdelord@dawan.fr>
2023-12-05 12:26:36 +0100

    Bootstrap, navbar, assets, scss, sass

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import './styles/app.scss';
+//import 'bootstrap/dist/css/bootstrap.css';
 
 require('bootstrap');
