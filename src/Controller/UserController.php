8970904a578e72a4efd3f8f4603a80b7bfcfbde9
DELORD Clément <cdelord@dawan.fr>
2023-12-06 11:43:31 +0100

    Auth

---
+++ src/Controller/UserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/register')]
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $hasher,
+        EntityManagerInterface $em
+    ): Response
+    {
+        $user = new User;
+
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $user->setPassword($hasher->hashPassword($user, $user->getPassword()));
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_user_login');
+        }
+
+        return $this->render('user/register.html.twig', [
+            'userForm' => $form    
+        ]);
+    }
+
+    #[Route('/login')]
+    public function login (): Response
+    {
+        return $this->render('User/login.html.twig');
+    }
+
+    #[Route('/logout')]
+    public function logout (): Response {
+        throw new \Exception('This code should not be reached, did you forget to add logout path in security.yaml ?');
+    }
+}
