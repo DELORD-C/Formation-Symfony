c772032a13b6513eedf16577dcfa68c7e49ad23f
DELORD Clément <cdelord@dawan.fr>
2024-01-22 16:28:48 +0100

    Form Type

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Form\PostType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,7 +14,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class PostController extends AbstractController
 {
     #[Route('/create')]
-    function create (): Response {
-        return $this->render('post/create.html.twig');
+    function create (Request $request, EntityManagerInterface $em): Response {
+
+        $post = new Post();
+
+        $form = $this->createForm(PostType::class, $post);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $em->persist($post);
+            $em->flush(); // Applique toutes les modifications en attente et vide la cache de la base de donnée
+        }
+
+        return $this->render('post/create.html.twig', [
+            'postForm' => $form
+        ]);
     }
 }
