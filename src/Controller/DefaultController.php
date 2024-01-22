908b66d421d273f18042a2a32626faeba869db72
DELORD Clément <cdelord@dawan.fr>
2024-01-22 14:12:46 +0100

    Templating

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,28 +2,37 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
     #[Route('/', methods: ['GET'])]
     function index (): Response {
-        return new Response('Hello World !');
+        return $this->render('default.html.twig', [
+            'text' => 'Hello World !'
+        ]);
     }
 
     #[Route('/random')]
     function random (): Response {
-        return new Response(rand(0, 100));
+        return $this->render('default.html.twig', [
+            'text' => rand(0, 100)
+        ]);
     }
 
     #[Route('/display/{value}', requirements: ['value' => '^[A-z]+$'])]
     function display ($value = 'Hello World !'): Response {
-        return new Response($value);
+        return $this->render('default.html.twig', [
+            'text' => $value
+        ]);
     }
 
     #[Route('/display/bienvenue', priority: 1)]
     function displayBienvenue (): Response {
-        return new Response('<h1>Bienvenue</h1>');
+        return $this->render('default.html.twig', [
+            'text' => '<h1>Bienvenue</h1>'
+        ]);
     }
 }
